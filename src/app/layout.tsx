584a058e4d0cc5ecd96a5a4568164726abfc101c
Sizar Corpse <sizarcorpse@gmail.com>
2023-11-29 13:24:10 +0600

    Project Boostiology

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Next Shadcn-ui Theme Boilerplate",
-  description: "A Boilerplate for Next.js with Shadcn-ui and Tailwind CSS",
+  title: "",
+  description: "",
 };
 
 export default function RootLayout({
