27e502e3ef57ec20f2b268c50e9590a39952fd89
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-01 22:03:55 +0600

    Add BParagraph component with optional animations and color variants

---
+++ src/components/elements/BParagraph.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { cn } from "@/libs/utils";
+import { cva } from "class-variance-authority";
+import { AnimationDefinition, motion, useAnimation } from "framer-motion";
+import React, { forwardRef, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+interface BParagraphProps {
+  size?: "default" | "xl" | "lg" | "sm" | "xs";
+  color?: "light" | "dark";
+  className?: string;
+  noAnimation?: boolean;
+  children?: React.ReactNode;
+  onAnimationStart?: (definition: AnimationDefinition) => void;
+}
+
+const BParagraphVariants = cva("font-inter leading-5", {
+  variants: {
+    size: {
+      default: "text-sm md:text-base",
+      xl: "text-lg md:text-xl",
+      lg: "text-base md:text-lg",
+      sm: "text-xs md:text-sm",
+      xs: "text-xs",
+    },
+    color: {
+      default: "text-foreground",
+      dark: "text-primary",
+      light: "text-secondary",
+    },
+  },
+  defaultVariants: {
+    color: "default",
+  },
+});
+
+const BParagraph = forwardRef<HTMLParagraphElement, BParagraphProps>(
+  ({ className, size, color, noAnimation, ...props }, ref) => {
+    const controls = useAnimation();
+    const { ref: inViewRef, inView } = useInView();
+
+    useEffect(() => {
+      if (inView && !noAnimation) {
+        controls.start("visible");
+      }
+    }, [controls, inView, noAnimation]);
+
+    const combinedRef = (node: HTMLParagraphElement) => {
+      if (typeof ref === "function") {
+        ref(node);
+      } else if (ref) {
+        ref.current = node;
+      }
+      inViewRef(node);
+    };
+
+    return (
+      <motion.p
+        ref={combinedRef}
+        className={cn(BParagraphVariants({ size, color, className }))}
+        initial={noAnimation ? undefined : "hidden"}
+        animate={noAnimation ? undefined : controls}
+        variants={
+          noAnimation
+            ? undefined
+            : {
+                hidden: { opacity: 0, y: 50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }
+        }
+        {...props}
+      />
+    );
+  }
+);
+
+BParagraph.displayName = "BParagraph";
+
+export default BParagraph;
