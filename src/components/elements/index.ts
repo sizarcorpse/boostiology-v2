2def1e3a6acc2a83c318d06a44d7c93208df623e
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-02 19:15:49 +0600

    Add indexes

---
+++ src/components/elements/index.ts
@@ -0,0 +1,10 @@
+export { default as BContainer } from "./BContainer";
+export { default as BGroupTitle } from "./BGroupTitle";
+export { default as BIcon } from "./BIcon";
+export type { BIconType } from "./BIcon";
+export { default as BIconBox } from "./BIconBox";
+export { default as BOfferCard } from "./BOfferCard";
+export { default as BParagraph } from "./BParagraph";
+export { default as BSection } from "./BSection";
+export { default as BSubtitle } from "./BSubtitle";
+export { default as BTitle } from "./BTitle";
---
+++ src/components/page-contents/index.ts
@@ -0,0 +1,3 @@
+export { default as HomeAbout } from "./HomeAbout";
+export { default as HomeHero } from "./HomeHero";
+export { default as HomeOffer } from "./HomeOffer";
