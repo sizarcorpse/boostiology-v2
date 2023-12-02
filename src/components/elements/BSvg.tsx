07b4b0338602c683d1f22e36082f65ead7d6c37a
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-02 19:07:20 +0600

    Add svgs

---
+++ src/components/elements/BSvg.tsx
@@ -0,0 +1,196 @@
+export const ArrowRightIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M3 5v14" />
+    <path d="M21 12H7" />
+    <path d="m15 18 6-6-6-6" />
+  </svg>
+);
+
+export const ArrowLeftIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M3 19V5" />
+    <path d="m13 6-6 6 6 6" />
+    <path d="M7 12h14" />
+  </svg>
+);
+
+export const SendIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="m3 3 3 9-3 9 19-9Z" />
+    <path d="M6 12h16" />
+  </svg>
+);
+
+export const ChartLineIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M3 3v18h18" />
+    <path d="m19 9-5 5-4-4-3 3" />
+  </svg>
+);
+
+export const TargetIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M21 11V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6" />
+    <path d="m12 12 4 10 1.7-4.3L22 16Z" />
+  </svg>
+);
+
+export const RocketIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z" />
+    <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" />
+    <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
+    <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
+  </svg>
+);
+
+export const BrainIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M12 4.5a2.5 2.5 0 0 0-4.96-.46 2.5 2.5 0 0 0-1.98 3 2.5 2.5 0 0 0-1.32 4.24 3 3 0 0 0 .34 5.58 2.5 2.5 0 0 0 2.96 3.08 2.5 2.5 0 0 0 4.91.05L12 20V4.5Z" />
+    <path d="M16 8V5c0-1.1.9-2 2-2" />
+    <path d="M12 13h4" />
+    <path d="M12 18h6a2 2 0 0 1 2 2v1" />
+    <path d="M12 8h8" />
+    <path d="M20.5 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z" />
+    <path d="M16.5 13a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z" />
+    <path d="M20.5 21a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z" />
+    <path d="M18.5 3a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z" />
+  </svg>
+);
+
+export const CloudIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle cx="12" cy="17" r="3" />
+    <path d="M4.2 15.1A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.2" />
+    <path d="m15.7 18.4-.9-.3" />
+    <path d="m9.2 15.9-.9-.3" />
+    <path d="m10.6 20.7.3-.9" />
+    <path d="m13.1 14.2.3-.9" />
+    <path d="m13.6 20.7-.4-1" />
+    <path d="m10.8 14.3-.4-1" />
+    <path d="m8.3 18.6 1-.4" />
+    <path d="m14.7 15.8 1-.4" />
+  </svg>
+);
+
+export const MonitorIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle cx="19" cy="6" r="3" />
+    <path d="M22 12v3a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h9" />
+    <path d="M12 17v4" />
+    <path d="M8 21h8" />
+  </svg>
+);
+
+export const OfferBgIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="100"
+    height="89"
+    viewBox="0 0 100 89"
+    fill="currentColor"
+    stroke="currentColor"
+  >
+    <path d="M89.4 20.166c1.18 1.266 1.85 2.913 1.86 4.58.012 1.667-.636 3.219-1.802 4.317l-6.741 6.316c1.169-1.095 2.76-1.644 4.424-1.524 1.664.12 3.264.896 4.448 2.16 1.184 1.264 1.856 2.911 1.866 4.58.011 1.668-.64 3.22-1.808 4.315l-2.208 2.068c1.263-.862 2.842-1.185 4.42-.903 1.578.282 3.038 1.147 4.086 2.423 1.047 1.275 1.605 2.865 1.56 4.45-.044 1.585-.688 3.047-1.8 4.093L66.422 86.349c-1.116 1.042-2.617 1.589-4.202 1.53-1.584-.059-3.135-.719-4.339-1.847-1.204-1.128-1.973-2.641-2.152-4.234-.178-1.593.247-3.148 1.19-4.352l-10.632 9.96c-1.169 1.095-2.76 1.643-4.424 1.523-1.664-.119-3.264-.896-4.448-2.16-1.184-1.264-1.855-2.911-1.866-4.58-.01-1.667.64-3.22 1.809-4.315l5.197-4.868c-1.015.945-2.35 1.484-3.782 1.527-1.432.043-2.873-.412-4.083-1.291a7.006 7.006 0 0 1-2.558-3.533c-.446-1.407-.407-2.889.11-4.195l-10.046 9.41c-1.169 1.096-2.76 1.644-4.424 1.524-1.664-.119-3.264-.896-4.448-2.16-1.184-1.264-1.855-2.911-1.866-4.58-.01-1.667.64-3.22 1.809-4.315l11.8-11.056c-1.169 1.096-2.76 1.644-4.424 1.524-1.664-.119-3.264-.896-4.448-2.16-1.184-1.264-1.855-2.911-1.866-4.58-.01-1.667.64-3.22 1.809-4.315l-4.702 4.405c-1.169 1.095-2.76 1.643-4.424 1.524-1.664-.12-3.264-.897-4.448-2.16C1.38 47.31.708 45.663.697 43.995c-.01-1.668.64-3.22 1.81-4.315L35.56 8.71c1.17-1.095 2.76-1.643 4.425-1.524 1.663.12 3.263.897 4.448 2.161 1.184 1.264 1.855 2.911 1.866 4.58.01 1.667-.64 3.22-1.809 4.315L61.496 2.312C62.664 1.217 64.256.668 65.92.788c1.664.12 3.264.896 4.448 2.16 1.184 1.264 1.855 2.911 1.866 4.58.01 1.668-.64 3.22-1.809 4.315l-5.197 4.869c1.014-.946 2.349-1.487 3.78-1.531 1.432-.044 2.874.41 4.084 1.288a7.007 7.007 0 0 1 2.561 3.532c.447 1.407.41 2.888-.106 4.195l4.98-4.666c1.172-1.09 2.763-1.635 4.425-1.516 1.662.12 3.261.893 4.448 2.152Z" />
+  </svg>
+);
