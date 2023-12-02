ac9e97704b0854de664ff769682df06cfb045141
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-02 19:14:00 +0600

    Add section HomeHero to homepage

---
+++ src/components/page-contents/HomeHero.tsx
@@ -0,0 +1,61 @@
+import {
+  BContainer,
+  BGroupTitle,
+  BIcon,
+  BIconType,
+  BParagraph,
+  BSection,
+  BSubtitle,
+  BTitle,
+} from "@/components/elements";
+import { HomeHeroProps } from "@/types/local";
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+import { Button } from "../ui/button";
+
+const HomeHero: FC<HomeHeroProps> = ({ heroData }) => {
+  const hero = heroData?.slides[0];
+
+  return (
+    <BSection
+      className="relative h-[80vh] overflow-hidden"
+      margins="exclude"
+      // style={{
+      //   backgroundImage: "url('/assets/images/image-1.jpg')",
+      //   backgroundSize: "cover",
+      //   backgroundPosition: "top",
+      // }}
+    >
+      <BContainer size="full" className="flex items-center">
+        <div className="max-w-[600px] space-y-7">
+          <BGroupTitle>
+            <BSubtitle size="large">{hero.subtitle}</BSubtitle>
+            <BTitle>{hero.title}</BTitle>
+          </BGroupTitle>
+          <BParagraph>{hero.description}</BParagraph>
+          <div className="flex flex-col items-start gap-0 sm:flex-row sm:gap-2">
+            {hero.buttons.map((button, index) => (
+              <Link href={button.href} key={index}>
+                <Button
+                  variant={button.variant}
+                  size="lg"
+                  title={button.title}
+                  className="mt-4 uppercase text-sm leading-4 gap-2"
+                >
+                  {button.label}
+                  <BIcon
+                    icon={button?.icon?.identifier as BIconType}
+                    className="w-5 h-5"
+                  />
+                </Button>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </BContainer>
+    </BSection>
+  );
+};
+
+export default HomeHero;
