833efcf1810ec8dd1780cce3ca3e70a5b4d7176e
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-02 19:14:32 +0600

    Add section HomeOffer to homepage

---
+++ src/components/page-contents/HomeOffer.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import {
+  BContainer,
+  BGroupTitle,
+  BIcon,
+  BIconBox,
+  BIconType,
+  BOfferCard,
+  BParagraph,
+  BSection,
+  BSubtitle,
+  BTitle,
+} from "@/components/elements";
+import { AnimatePresence, motion, useAnimation } from "framer-motion";
+
+const HomeOffer = ({ offerData: offer }: any) => {
+  return (
+    <BSection margins="exclude">
+      <BContainer className="flex flex-col gap-20">
+        <div className="flex flex-col items-stretch justify-stretch gap-8 max-w-screen-md text-center mx-auto">
+          <BGroupTitle alignment="center">
+            <BSubtitle size="large">{offer.subtitle}</BSubtitle>
+            <BTitle component="h2" size="lg">
+              {offer.title}
+            </BTitle>
+          </BGroupTitle>
+          <BParagraph>{offer.description}</BParagraph>
+        </div>
+
+        <div className="mx-auto grid grid-cols-[repeat(auto-fill,minmax(286px,1fr))] gap-7 justify-items-end justify-center w-full md:max-w-2xl lg:max-w-5xl xl:max-w-full">
+          {offer.offers.map((item: any, index: number) => (
+            <BOfferCard key={index} offer={item} index={index} />
+          ))}
+        </div>
+      </BContainer>
+    </BSection>
+  );
+};
+
+export default HomeOffer;
