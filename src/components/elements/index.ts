90dea76e6aa3feabb0fefce1730e40c02d4722da
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-03 20:29:00 +0600

    Update package, indexes, assets

--- src/components/elements/index.ts
+++ src/components/elements/index.ts
@@ -1,3 +1,4 @@
+export { default as BCarousel } from "./BCarousel";
 export { default as BContainer } from "./BContainer";
 export { default as BGroupTitle } from "./BGroupTitle";
 export { default as BIcon } from "./BIcon";
@@ -5,6 +6,7 @@ export type { BIconType } from "./BIcon";
 export { default as BIconBox } from "./BIconBox";
 export { default as BOfferCard } from "./BOfferCard";
 export { default as BParagraph } from "./BParagraph";
+export { default as BProjectCard } from "./BProjectCard";
 export { default as BSection } from "./BSection";
 export { default as BSubtitle } from "./BSubtitle";
 export { default as BTitle } from "./BTitle";
--- src/components/page-contents/index.ts
+++ src/components/page-contents/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeAbout } from "./HomeAbout";
 export { default as HomeHero } from "./HomeHero";
 export { default as HomeOffer } from "./HomeOffer";
+export { default as HomeProject } from "./HomeProject";
