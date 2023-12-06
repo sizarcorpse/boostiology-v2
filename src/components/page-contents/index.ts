c24376361a3a32b71f5eaf9ad7e97969e9ad6d51
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-06 15:52:15 +0600

    Add assets,update indexes

--- src/components/page-contents/index.ts
+++ src/components/page-contents/index.ts
@@ -3,4 +3,6 @@ export { default as HomeBrand } from "./HomeBrand";
 export { default as HomeHero } from "./HomeHero";
 export { default as HomeOffer } from "./HomeOffer";
 export { default as HomeProject } from "./HomeProject";
+export { default as HomeReview } from "./HomeReview";
 export { default as HomeTeam } from "./HomeTeam";
+export { default as HomeWhy } from "./HomeWhy";
