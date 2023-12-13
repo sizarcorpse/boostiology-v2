83a01276d5548926b2ac54af1badb7843f4575c4
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-13 21:21:59 +0600

    Add section ServiceDescription for service page

---
+++ src/components/page-contents/ServiceDescription.tsx
@@ -0,0 +1,19 @@
+import { BContainer, BParagraph, BSection } from "@/components/elements";
+
+const ServiceDescription = ({ serviceDescriptionData: description }: any) => {
+  return (
+    <BSection margins="exclude">
+      <BContainer>
+        <article className="space-y-6">
+          {description?.map((item: any, index: number) => (
+            <BParagraph key={index} noAnimation size="lg">
+              {item.d}
+            </BParagraph>
+          ))}
+        </article>
+      </BContainer>
+    </BSection>
+  );
+};
+
+export default ServiceDescription;
