c67e3b551482241c4433b08c2c014a4d0b970753
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-17 15:47:53 +0600

    Fix nextconfig

--- next.config.js
+++ next.config.js
@@ -7,8 +7,11 @@ const nextConfig = {
   },
 };
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+let withBundleAnalyzer = (config) => config;
 
+if (process.env.ANALYZE) {
+  withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+  });
+}
 module.exports = withBundleAnalyzer(nextConfig);
