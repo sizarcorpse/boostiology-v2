2e1b07567e15163bdf72449498322beae5cc8bb8
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-17 15:15:29 +0600

    Update BIconBox component

--- src/components/elements/BIconBox.tsx
+++ src/components/elements/BIconBox.tsx
@@ -1,55 +1,112 @@
 import { BIcon, BParagraph, BTitle } from "@/components/elements";
 import { cn } from "@/libs/utils";
-import { FC } from "react";
+import { FC, forwardRef } from "react";
+
 interface BIconBoxProps {
   icon: string;
   title?: string;
   description?: string;
   orientation?: "vertical" | "horizontal";
-  size?: "md" | "lg";
   component?: "h3" | "h4" | "h5" | "h6";
+  noAnimation?: boolean;
+  className?: string;
 }
 
-const BIconBox: FC<BIconBoxProps> = ({
-  icon,
-  title,
-  description,
-  orientation = "vertical",
-  size = "md",
-  component = "h4",
-}) => {
-  const isVertical = orientation === "vertical";
-  const computedSize = isVertical ? "lg" : "md";
-
-  return (
-    <div
-      className={`group flex flex-${
-        isVertical ? "col" : "row"
-      } items-start justify-center gap-4`}
-    >
+const BIconBoxVertical = forwardRef<HTMLDivElement, BIconBoxProps>(
+  (props, ref) => {
+    return (
       <div
-        className={`basis-19 aspect-square rounded-lg bg-accent/20 p-4 shrink-0 ${
-          computedSize === "md" ? " md:p-4" : " md:p-5"
-        }`}
+        className={cn(
+          `group flex flex-col items-start gap-4 overflow-hidden`,
+          props.className
+        )}
+        ref={ref}
       >
-        <BIcon icon={icon} className="w-7 h-7 text-accent md:w-9 md:h-9" />
-      </div>
-      {(title || description) && (
-        <div className="flex flex-col grow">
-          {title && (
-            <BTitle
-              component={component}
-              size={computedSize === "md" ? "xs" : "sm"}
-              className="font-semibold"
-            >
-              {title}
-            </BTitle>
-          )}
-          {description && <BParagraph>{description}</BParagraph>}
+        <div className={`aspect-square rounded-lg bg-accent/20 p-4 shrink-0`}>
+          <BIcon
+            icon={props.icon}
+            className="w-7 h-7 text-accent md:w-9 md:h-9"
+          />
         </div>
-      )}
-    </div>
-  );
-};
+        {(props.title || props.description) && (
+          <div className="flex flex-col gap-2">
+            {props.title && (
+              <BTitle
+                component={props.component}
+                size="sm"
+                className="font-semibold"
+                {...(props.noAnimation && { noAnimation: true })}
+              >
+                {props.title}
+              </BTitle>
+            )}
+            {props.description && (
+              <BParagraph {...(props.noAnimation && { noAnimation: true })}>
+                {props.description}
+              </BParagraph>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
+
+const BIconBoxHorizontal = forwardRef<HTMLDivElement, BIconBoxProps>(
+  (props, ref) => {
+    return (
+      <div
+        className={cn(
+          `group flex flex-row items-start justify-start gap-4`,
+          props.className
+        )}
+        ref={ref}
+      >
+        <div
+          className={`basis-19 aspect-square rounded-lg bg-accent/20 p-4 shrink-0`}
+        >
+          <BIcon
+            icon={props.icon}
+            className="w-7 h-7 text-accent md:w-9 md:h-9"
+          />
+        </div>
+        {(props.title || props.description) && (
+          <div className="flex flex-col gap-0">
+            {props.title && (
+              <BTitle
+                component={props.component}
+                size="xs"
+                className="font-semibold"
+                {...(props.noAnimation && { noAnimation: true })}
+              >
+                {props.title}
+              </BTitle>
+            )}
+            {props.description && (
+              <BParagraph {...(props.noAnimation && { noAnimation: true })}>
+                {props.description}
+              </BParagraph>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
+
+const BIconBox = forwardRef<HTMLDivElement, BIconBoxProps>((props, ref) => {
+  const orientation =
+    props.orientation === "horizontal" ? "horizontal" : "vertical";
+
+  if (orientation === "vertical") {
+    return <BIconBoxVertical {...props} ref={ref} />;
+  }
+
+  return <BIconBoxHorizontal {...props} ref={ref} />;
+});
+
+BIconBox.displayName = "BIconBox";
+BIconBoxVertical.displayName = "BIconBoxVertical";
+BIconBoxHorizontal.displayName = "BIconBoxHorizontal";
 
 export default BIconBox;
